75c5c08ef7a057f47222cc6fe1a540a267a5faaf
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-26 18:17:10 +0300

    commit tree 785453aa2b87353226aa828eded85ed5a54f8743

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
-ENV["RACK_ENV"] ||= "development"
+ENV["RACK_ENV"] ||= "development" #specify that we are using the development database
 
 require 'bundler/setup'
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require(:default, ENV["RACK_ENV"])  #load all the gems in the gemfile and make them available for the entre progrm in an environment folder
 
 require_all 'app/models'
---
+++ db/migrate/20220826140509_create_costumes.rb
@@ -0,0 +1,14 @@
+class CreateCostumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table(:costumes) do |t|
+      t.string(:name)
+      t.float(:price)
+      t.string(:size)
+      t.string(:image_url)
+      t.timestamps
+    end
+  end
+end
+
+
+#the migrations inherit from the activerecord migrations class
